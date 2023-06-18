bf40b7b1c3751eeb1a0ea56928974e219eed4ea6
Erika Laamanen <erika.laamanen@gmail.com>
2023-06-18 17:31:08 +0300

    Add storybook and basic colors and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      lemon: "rgb(var(--color-lemon) / <alpha-value>)",
+      grape: "rgb(var(--color-grape) / <alpha-value>)",
+      fuschia: "rgb(var(--color-fuschia) / <alpha-value>)",
+      blue: "rgb(var(--color-blue) / <alpha-value>)",
+      lightGray: "rgb(var(--color-light-gray) / <alpha-value>)",
+      cream: "rgb(var(--color-cream) / <alpha-value>)",
+      black: "black",
+      white: "white",
+      red: {
+        default: colors.red["500"],
+        100: colors.red["100"],
+        300: colors.red["300"],
+        700: colors.red["700"],
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      borderWidth: {
+        1: "1.5px",
+      },
+      minWidth: {
+        0: "0",
+        6: "6rem",
       },
     },
   },
   plugins: [],
-}
+};
