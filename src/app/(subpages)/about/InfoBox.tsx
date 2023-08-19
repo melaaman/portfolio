999bb1a4f7924a7607cdca12ca859a19636b99c4
Erika Laamanen <erika.laamanen@gmail.com>
2023-08-19 16:47:58 +0300

    Add sizes to InfoBox images

--- src/app/(subpages)/about/InfoBox.tsx
+++ src/app/(subpages)/about/InfoBox.tsx
@@ -1,15 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import { InfoBoxItem } from "./types";
+import Image from "next/image";
 
 export const InfoBox = ({ item }: { item: InfoBoxItem }) => {
   return (
     <div className="InfoBox flex flex-col md:flex-row gap-5 md:gap-8 items-center">
-      <img
+      <Image
         className="InfoBox-image"
-        src={item.image.fields?.file?.url}
+        src={item.image.fields?.file?.url ?? ""}
         alt={`image-${item.order}`}
         width={150}
         height={100}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       />
       <p className="InfoBox-description">{item.description}</p>
     </div>
